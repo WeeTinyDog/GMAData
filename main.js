3d82679d3a77a8454dcdf8181dd4d523da351bf5
Adam Crawford <ajimcrawford@gmail.com>
2021-02-24 16:01:07 +0000

    Resize works properly

--- main.js
+++ main.js
@@ -5,9 +5,9 @@ import * as THREE from '/three/build/three.module.js';
 import {GLTFLoader} from '/three/examples/jsm/loaders/GLTFLoader.js';
 import {OrbitControls} from '/three/examples/jsm/controls/OrbitControls.js';
 import { RGBELoader } from '/three/examples/jsm/loaders/RGBELoader.js';
-import { RoughnessMipmapper } from '/three/examples/jsm/utils/RoughnessMipmapper.js';
 
-let scene, camera, renderer;
+
+let scene, camera, renderer; //global so can be referenced wherever
 
 init();
 
@@ -17,7 +17,7 @@ function init() {
     scene = new THREE.Scene();
 
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight,0.1, 1000);
-    new RGBELoader()
+    new RGBELoader() //load HDRI
     .setDataType( THREE.UnsignedByteType )
     .setPath( 'Textures/' )
     .load( 'lilienstein_1k.hdr', function ( texture ) {
@@ -32,33 +32,18 @@ function init() {
 
         render();
 
-        // model
-
-        // use of RoughnessMipmapper is optional
-        const roughnessMipmapper = new RoughnessMipmapper( renderer );
-
+        //LOAD MODELS HERE
         const loader = new GLTFLoader().setPath( 'Meshes/' );
         loader.load( 'glasgow.gltf', function ( gltf ) {
-
-            gltf.scene.traverse( function ( child ) {
-
-                if ( child.isMesh ) {
-
-                    // TOFIX RoughnessMipmapper seems to be broken with WebGL 2.0
-                    // roughnessMipmapper.generateMipmaps( child.material );
-
-                }
-
-            } );
-
             scene.add( gltf.scene );
 
-            roughnessMipmapper.dispose();
+
 
             render();
 
         } );
     });
+    //RENDERER SETTINGS
     renderer = new THREE.WebGLRenderer( { antialias: true } );
 	renderer.setPixelRatio( window.devicePixelRatio );
 	renderer.setSize( window.innerWidth, window.innerHeight );
@@ -68,15 +53,17 @@ function init() {
 
     document.body.appendChild(renderer.domElement);
 
-
+    //NEEDED TO GENERATE HDRI
     const pmremGenerator = new THREE.PMREMGenerator( renderer );
 	pmremGenerator.compileEquirectangularShader();
    
+    //DEFAULT ORBIT CONTROLS: TO DO: CONSTRAIN SO YOU CAN'T GO UNDER MAP
     const controls = new OrbitControls( camera, renderer.domElement );
     controls.addEventListener( 'change', render ); // use if there is no animation loop
     controls.target.set( 0, 0, - 0.2 );
 
     camera.position.set( - 1.8, 1.5, 2.7 );
+    //REmember to call this after a manual move!
     controls.update();
 
     window.addEventListener('resize', onWindowResize, false);
@@ -98,6 +85,7 @@ function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
+    render();
     
 }
 
