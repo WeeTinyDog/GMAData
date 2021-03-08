f3329d0136531df8ea83d50a92bf1baf64d5d4d5
Adam Crawford <ajimcrawford@gmail.com>
2021-03-08 14:15:46 +0000

    updated with current stats

--- main.js
+++ main.js
@@ -37,218 +37,230 @@ let postcodes = [
     {
         "identifier":"g1",
         "displayName" : "G1",
-        "requests": 12,
-        "fulfilledRequests":100
+        "requests": 7,
+
     },
     {
         "identifier":"g3",
         "displayName" : "G3",
-        "requests": 114,
-        "fulfilledRequests":100
+        "requests": 53,
+
     },
     {
         "identifier":"g4",
         "displayName" : "G4",
-        "requests": 0,
-        "fulfilledRequests":100
+        "requests": 10,
+
     },
     {
         "identifier":"g5",
         "displayName" : "G5",
-        "requests": 3,
-        "fulfilledRequests":100
+        "requests": 31,
+
     },
     {
         "identifier":"g11",
         "displayName" : "G11",
-        "requests": 145,
-        "fulfilledRequests":100
+        "requests": 23,
+
     },
     {
         "identifier":"g12",
         "displayName" : "G12",
-        "requests": 166,
-        "fulfilledRequests":100
+        "requests": 16,
+
     },
     {
         "identifier":"g13",
         "displayName" : "G13",
-        "requests": 666,
-        "fulfilledRequests":100
+        "requests": 79,
+
     },
     {
         "identifier":"g14",
         "displayName" : "G14",
-        "requests": 363,
-        "fulfilledRequests":100
+        "requests": 29,
+
     },
     {
         "identifier":"g15",
         "displayName" : "G15",
-        "requests": 453,
-        "fulfilledRequests":100
+        "requests": 50,
+
     },
     {
         "identifier":"g20",
         "displayName" : "G20",
-        "requests": 786,
-        "fulfilledRequests":100
+        "requests": 63,
+
     },
     {
         "identifier":"g21",
         "displayName" : "G21",
-        "requests": 423,
-        "fulfilledRequests":100
+        "requests": 72,
+
     },
     {
         "identifier":"g22",
         "displayName" : "G22",
-        "requests": 456,
-        "fulfilledRequests":100
+        "requests": 21,
+
     },
     {
         "identifier":"g23",
         "displayName" : "G23",
-        "requests": 42,
-        "fulfilledRequests":100
+        "requests": 10,
+
     },
     {
         "identifier":"g31",
         "displayName" : "G31",
-        "requests": 423,
-        "fulfilledRequests":100
+        "requests": 35,
+
     },
     {
         "identifier":"g32",
         "displayName" : "G32",
-        "requests": 44,
-        "fulfilledRequests":100
+        "requests": 59,
+
     },
     {
         "identifier":"g33",
         "displayName" : "G33",
-        "requests": 644,
-        "fulfilledRequests":100
+        "requests": 61,
+
     },
     {
         "identifier":"g34",
         "displayName" : "G34",
-        "requests": 144,
-        "fulfilledRequests":100
+        "requests": 26,
+
     },
     {
         "identifier":"g40",
         "displayName" : "G40",
-        "requests": 1,
-        "fulfilledRequests":100
+        "requests": 17,
+
     },
     {
         "identifier":"g41",
         "displayName" : "G41",
-        "requests": 145,
-        "fulfilledRequests":100
+        "requests": 123,
+
     },
     {
         "identifier":"g42",
         "displayName" : "G42",
-        "requests": 44,
-        "fulfilledRequests":100
+        "requests": 73,
+
     },
     {
         "identifier":"g43",
         "displayName" : "G43",
-        "requests": 146,
-        "fulfilledRequests":100
+        "requests": 27,
+
     },
     {
         "identifier":"g44",
         "displayName" : "G44",
-        "requests": 146,
-        "fulfilledRequests":100
+        "requests": 9,
+
     },
     {
         "identifier":"g45",
         "displayName" : "G45",
-        "requests": 111,
-        "fulfilledRequests":100
+        "requests": 31,
+
     },
     {
         "identifier":"g46",
         "displayName" : "G46",
-        "requests": 464,
-        "fulfilledRequests":100
+        "requests": 8,
+
     },
     {
         "identifier":"g51",
         "displayName" : "G51",
-        "requests": 464,
-        "fulfilledRequests":100
+        "requests": 104,
+
     },
     {
         "identifier":"g52",
         "displayName" : "G52",
-        "requests": 11,
-        "fulfilledRequests":100
+        "requests": 64,
+
     },
     {
         "identifier":"g53",
         "displayName" : "G53",
-        "requests": 456,
-        "fulfilledRequests":100
+        "requests": 26,
+
+    },
+    {
+        "identifier":"g61",
+        "displayName" : "G61",
+        "requests": 2,
+
+    },
+    {
+        "identifier":"g64",
+        "displayName" : "G64",
+        "requests": 4,
+
     },
     {
         "identifier":"g66",
         "displayName" : "G66",
-        "requests": 112,
-        "fulfilledRequests":100
+        "requests": 2,
+
     },
     {
         "identifier":"g69",
         "displayName" : "G69",
-        "requests": 489,
-        "fulfilledRequests":100
+        "requests": 7,
+
     },
     {
         "identifier":"g72",
         "displayName" : "G72",
-        "requests": 422,
-        "fulfilledRequests":100
+        "requests": 7,
+
     },
     {
         "identifier":"g73",
         "displayName" : "G73",
-        "requests": 111,
-        "fulfilledRequests":100
+        "requests": 28,
+
     },
     {
         "identifier":"g74",
         "displayName" : "G74",
-        "requests": 445,
-        "fulfilledRequests":100
+        "requests": 6,
+
     },
     {
         "identifier":"g78",
         "displayName" : "G78",
-        "requests": 53,
-        "fulfilledRequests":100
+        "requests": 2,
+
     },
     {
         "identifier":"g81",
         "displayName" : "G81",
-        "requests": 53,
-        "fulfilledRequests":100
+        "requests": 8,
+
     },
     {
         "identifier":"g82",
-        "displayName" : "G82",
-        "requests": 44,
-        "fulfilledRequests":100
+        "displayName" : "G82-4",
+        "requests": 10,
+
     },
     {
         "identifier":"ml4",
-        "displayName" : "ML4",
-        "requests": 11,
-        "fulfilledRequests":100
+        "displayName" : "ML Area",
+        "requests": 2,
+
     }
 ]
 
@@ -269,16 +281,16 @@ function init() {
     scene = new THREE.Scene();
     {
         const color ="aliceblue";
-        const near = 10;
-        const far = 20;
+        const near = 25;
+        const far = 30;
         scene.fog = new THREE.Fog(color, near, far);
         scene.background = new THREE.Color(color)
     }
     const light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
     scene.add( light );
 
-    const loc = new THREE.Vector3(0,3,0);
-    createMeshText("Testing text", loc);
+    //const loc = new THREE.Vector3(0,3,0);
+    //createMeshText("Testing text", loc);
 
 
     camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight,0.1, 1000);
@@ -314,7 +326,7 @@ function init() {
                     mat.roughness = 0.02;
                     mat.transparent = true;
                     child.material = mat;
-                    child.scale.y = percent+0.01;
+                    child.scale.y = percent*9+0.01;
                     objects.push(child);
                     }   
             });
@@ -341,8 +353,8 @@ function init() {
    // controls.maxAzimuthAngle = 0;
    // controls.minAzimuthAngle = 0;
     controls.enablePan = true;
-    //controls.minDistance = 5;
-    //controls.maxDistance = 20;
+    controls.minDistance = 5;
+    controls.maxDistance = 20;
     controls.enableDamping = true;
     controls.dampingFactor =0.1;
     controls.enabled = true;
@@ -538,6 +550,8 @@ function onclick(event) {
     controls.enabled = false;
     controls.target.x = target.x;
     controls.target.z = target.z;
+    tween.easing(TWEEN.Easing.Quadratic.In);
+    tweenR.easing(TWEEN.Easing.Quadratic.In)
    
     tween.start();
     tweenR.start();
